9d9ff62113c4039874fef6e48c35b687ce722f01
andrew cohen <cohen.andrew.n@gmail.com>
2023-11-10 20:10:06 -0500

    fix: add logs

--- code/controller/mural/service/service.go
+++ code/controller/mural/service/service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"fmt"
+	"log/slog"
 	"mural/db"
 	"mural/model"
 	"time"
@@ -122,6 +123,7 @@ func GetCurrentDecade() string {
 	current_day := time.Now().Weekday()
 	loc, err := time.LoadLocation("America/New_York")
 	if err != nil {
+		slog.Debug(err.Error())
 		current_day = time.Now().In(loc).Weekday()
 	} 
 
