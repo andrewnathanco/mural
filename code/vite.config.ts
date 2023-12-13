2a6b6c57cb9c82c088387595b82961f282a449d3
andrew cohen <cohen.andrew.n@gmail.com>
2023-12-13 01:01:07 -0500

    feat: test?

--- code/vite.config.ts
+++ code/vite.config.ts
@@ -2,5 +2,9 @@ import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
   plugins: [solid({ ssr: false })],
 });
